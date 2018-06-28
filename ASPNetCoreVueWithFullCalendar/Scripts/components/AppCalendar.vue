941f3e70724f54797c23ed06a637acd824bd92e7
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-06-28 22:32:33 +0800

    Add project files.

---
+++ ASPNetCoreVueWithFullCalendar/Scripts/components/AppCalendar.vue
@@ -0,0 +1,19 @@
+﻿<template>
+    <div>
+        <app-calendar></app-calendar>
+    </div>
+</template>
+
+<script>
+    import Calendar from "../components/Calendar.vue";
+
+    export default {
+        components: {
+            appCalendar: Calendar
+        }
+    }
+</script>
+
+<style>
+    @import "~fullcalendar/dist/fullcalendar.min.css";
+</style>
---
+++ ASPNetCoreVueWithFullCalendar/webpack.config.js
@@ -0,0 +1,47 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+
+module.exports = [
+    {
+        entry: './Scripts/calendar.js',
+        output: {
+            path: path.join(__dirname, "/wwwroot/js/"),
+            filename: 'calendar.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['babel-preset-env']
+                        }
+                    }
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                    options: {
+                        // `vue-loader` options
+
+                    }
+                },
+                { test: /\.css$/, use: 'css-loader' },
+                { test: /\.scss$/, use: 'sass-loader' },
+                { test: /\.ts$/, use: 'ts-loader' }
+            ]
+        },
+        resolve: {
+            alias: {
+                vue: "vue/dist/vue.js"
+            }
+        },
+        mode: 'development',
+        plugins: [
+            new webpack.NamedModulesPlugin(),
+            new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") })
+        ]
+    }
+];
